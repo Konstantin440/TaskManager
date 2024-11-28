a0ec608529a92913d765138dee73572b6ae6dd68
DmitriyRND <dim3333333333@yandex.ru>
2024-11-28 19:48:48 +0300

    add service CRUD method

--- src/main/java/org/example/taskmanager/service/UserService.java
+++ src/main/java/org/example/taskmanager/service/UserService.java
@@ -5,6 +5,8 @@ import org.example.taskmanager.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 
 public class UserService  {
@@ -12,6 +14,24 @@ public class UserService  {
     private UserRepository userRepository;
 
     public User save(User user) {
-      return  userRepository.save(user);
+
+        return  userRepository.save(user);
     }
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+    public void deleteUserById(Long id) {
+        userRepository.deleteById(id);
+    }
+    public User updateUser(Long id,User user) {
+        User existingUser = getUserById(id);
+        existingUser.setName(user.getName());
+        return userRepository.save(existingUser);
+
+
+    }
+
 }
