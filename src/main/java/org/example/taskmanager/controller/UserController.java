0375f59269ed07432c56a3b7afd0565c31029695
Konstantin440 <kosmodrom62@gmail.com>
2024-12-23 17:20:44 +0300

    test

--- src/main/java/org/example/taskmanager/controller/UserController.java
+++ src/main/java/org/example/taskmanager/controller/UserController.java
@@ -4,8 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.example.taskmanager.entity.User;
 import org.example.taskmanager.service.UserService;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -16,9 +17,36 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/")
-    public User addUser (@RequestBody User user) {
+    public User addUser(@RequestBody User user) {
         return userService.save(user);
 
     }
 
+
+    @DeleteMapping("/deleteUser/")
+    public void deleteUser(User user) {
+        userService.deleteUser(user);
+    }
+
+    @GetMapping("/getAllUsers")
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @DeleteMapping("/deleteAllUsers")
+    public void deleteAllUsers() {
+        List<User> users = userService.getAllUsers();
+        users.clear();
+    }
+
+    @PatchMapping("/changeUserName")
+    public User changeUserName(@RequestParam long id, @RequestParam String name) {
+        User user = userService.getUserById(id);
+        user.setName(name);
+        return userService.save(user);
+    }
 }
+
+
+
+
--- src/main/java/org/example/taskmanager/service/UserService.java
+++ src/main/java/org/example/taskmanager/service/UserService.java
@@ -11,28 +11,38 @@ import java.util.List;
 
 
 @Service
-public class UserService  {
+public class UserService {
     @Autowired
     private UserRepository userRepository;
 
     public User save(User user) {
 
-        return  userRepository.save(user);
+        return userRepository.save(user);
     }
+
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
+
     public User getUserById(Long id) {
         return userRepository.findById(id).orElse(null);
     }
+
     public void deleteUserById(Long id) {
         userRepository.deleteById(id);
     }
-    public User updateUser(Long id,User user) {
+
+
+    public User changeUserName(Long id, User user) {
         User existingUser = getUserById(id);
         existingUser.setName(user.getName());
         return userRepository.save(existingUser);
 
+    }
+
+
+    public void deleteUser(User user) {
+        userRepository.delete(user);
     }
 
 }
